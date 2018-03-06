c1cdbc7becfbcedc7580af6279375e9922fc706d
mintdash <mintdash@users.noreply.github.com>
2018-03-06 16:59:56 +0900

    Create isitodd.py

---
+++ isitodd.py
@@ -0,0 +1,9 @@
+num = input("Please type an integer: ")
+if num%4 == 0:
+    print ("Your number is multiple of four.")
+elif num%2 ==0:
+    print("Your number is an even number.")
+elif num%2 == 1:
+    print("Your chose an odd number.")
+else:
+    print("What were you thinking?")
